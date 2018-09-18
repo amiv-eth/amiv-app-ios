6a8e6c0da8385312ae8d2b2bff833fed6a0d8a41
Domenic Wüthrich <domenic.software@gmail.com>
2018-09-18 19:11:04 +0200

    Initial Commit

---
+++ AmivTests/AmivTests.swift
@@ -0,0 +1,34 @@
+//
+//  AmivTests.swift
+//  AmivTests
+//
+//  Created by Domenic Wüthrich on 18.09.18.
+//  Copyright © 2018 Amiv an der ETH. All rights reserved.
+//
+
+import XCTest
+@testable import Amiv
+
+class AmivTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
